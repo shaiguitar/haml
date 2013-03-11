322ee6f5ee9c643dc345176ed0ff4289bb63018a
Shai Rosenfeld <srosenfeld@engineyard.com>
2013-03-10 22:18:42 -0700

    more specs

--- lib/haml/find_text.rb
+++ lib/haml/find_text.rb
@@ -1,32 +1,78 @@
 class FindText
+
   def initialize(haml)
-    @haml = haml
+    @parser = Haml::Parser.new(haml, Haml::Options.new)
   end
 
   def run
-    match = @haml.rstrip.scan(/(([ \t]+)?(.*?))(?:\Z|\r\n|\r|\n)/m)
-    match.pop
-    @template = match.each_with_index.map do |(full, whitespace, text), index|
-      Haml::Parser::Line.new(whitespace, text.rstrip, full, index, self, false)
-    end
+    template = @parser.instance_variable_get(:@template) # HAX copy over Haml::Parser.new way of doing it instead of relying on internals
     result = []
-    @template.map{|line|
-      if txt = plain_text?(line)
+    template.each{|line|
+      if txt = plain_text(line)
         result << txt
       end
-     }.compact
+     }
+     result.compact
   end
 
-  THINGS_THAT_ARE_NOT_TEXT = [ Haml::Parser::DIV_CLASS, Haml::Parser::DIV_ID, Haml::Parser::ELEMENT,
-      Haml::Parser::COMMENT, Haml::Parser::SANITIZE, Haml::Parser::SCRIPT, Haml::Parser::FLAT_SCRIPT,
-      Haml::Parser::SILENT_SCRIPT, Haml::Parser::FILTER, Haml::Parser::DOCTYPE, Haml::Parser::ESCAPE ]
+  THINGS_THAT_ARE_NOT_TEXT = [ Haml::Parser::DIV_CLASS, Haml::Parser::DIV_ID,
+      Haml::Parser::COMMENT, Haml::Parser::SANITIZE, Haml::Parser::FLAT_SCRIPT,
+      Haml::Parser::FILTER, Haml::Parser::DOCTYPE, Haml::Parser::ESCAPE ]
 
-  def plain_text?(line)
+  ## hint: Use http://rubular.com
+  SIMPLE_STRING_REGEX = /["'](.*)["']/
+  # = "yes"
+  # = 'yes'
+  LINK_TO_REGEX = /link_to\s*\(?\s*['"](.*)['"]\s*,\s*(.*)\)?/
+  # = link_to "yes", "http://bla.com"
+  # = link_to('yes'    , "http://bla.com")
+  # = link_to(  "yes's w space", "http://bla.com")
+  ELEMENT_REGEX = /%([\w.#-]+)({.+?})?(=)?(.*)/
+  # %foo.bar yes
+  # %foo.bar= no
+  # %foo{:a => 'b'} yes
+  # rubular.com against %foo#bar#cheez{:cheeze => 'hell'}= "what #{var}"
+  
+  def plain_text(line)
     case line.text[0]
     when *THINGS_THAT_ARE_NOT_TEXT
       nil
+    when Haml::Parser::SILENT_SCRIPT
+      parse_silent_script(line)
+    when Haml::Parser::ELEMENT
+      parse_element(line)
+    when Haml::Parser::SCRIPT
+      parse_loud_script(line)
     else
       line.text
     end
   end
+  
+  private
+
+  def parse_silent_script(line)
+    line.text.match(/-[\s\t]*#{SIMPLE_STRING_REGEX}/) && $1
+  end
+  
+  def parse_loud_script(line)
+    line.text.match(/=[\s\t]*#{LINK_TO_REGEX}/)
+    return $1 if text = $1
+    line.text.match(/=[\s\t]*#{SIMPLE_STRING_REGEX}/)
+    return $1
+  end
+  
+  def parse_element(line)
+    line.text.match(ELEMENT_REGEX)
+    elem_with_class_and_ids = $1
+    attributes_ruby_style = $2
+    is_loud_script = $3
+    text = $4
+    if is_loud_script
+      parser = Haml::Parser.new("= #{text}", Haml::Options.new)
+      parse_loud_script(parser.instance_variable_get(:@template).first) # just treat it like a loud script
+    else
+      text.strip
+    end
+  end
+  
 end
--- test/string_finder_test.rb
+++ test/string_finder_test.rb
@@ -3,56 +3,137 @@ require 'test_helper'
 module Haml
   class StringFinderTest < MiniTest::Unit::TestCase
 
-    test "ignores silent scripts" do
-      begin
-        result = find_text "- if true\n  - case @foo\n  - when 1\n    bar\n- else\n  bar"
-        assert_equal result, []
-      rescue SyntaxError
-        flunk 'else clause after if containing case should be accepted'
-      end
+    # random tests from before used, just leaving them for now, cleanup later
+    # test "finds all the text in a statement" do
+    #   result = find_text "- if true\n  - case @foo\n  - when 1\n    cheese\n- else\n  rice"
+    #   assert_equal result, ["cheese", "rice"]
+    # end
+    # 
+    # test "finds all the text in a statement" do
+    #   result = find_text "- if true\n  - unless @foo\n  blue\n- else\n  green"
+    #   assert_equal result, ["blue", "green"]
+    # end
+    # 
+    # test "finds silent script with strings" do
+    #   result = find_text "= if true\n  - 'hank'\n-else\n  - 'bob'"
+    #   assert_equal result, ["hank", "bob"]
+    # end
+    # 
+    # test "also this" do
+    #   result = find_text "- foo = 1\n-case foo\n  -when 1\n    crown\n  -else\n    brown"
+    #   assert_equal result, ["crown", "brown"]
+    # end
+
+    # regular text mode
+    test "silent scripts with strings and regular text" do
+      assert_equal find_text("iphone"), ["iphone"]
+      assert_equal find_text("  iphone4"), ["iphone4"]
     end
 
-    test "else after if containing unless is accepted" do
-      begin
-        result = find_text "- if true\n  - unless @foo\n  bar\n- else\n  bar"
-        assert_equal result, true
-      rescue SyntaxError
-        flunk 'else clause after if containing unless should be accepted'
-      end
+
+    # silent script mode
+    test "silent scripts with strings and regular text" do
+      assert_equal find_text("-if true\n  =if true\n    - 'jessica'\n-else\n  sheen"), ["jessica", "sheen"]
     end
     
-    test "loud script with else is accepted" do
-      begin
-        result = find_text "= if true\n  - 'A'\n-else\n  - 'B'"
-        assert_equal result, true
-      rescue SyntaxError
-        flunk 'loud script (=) should allow else'
-      end
+    # html element mode
+    test "%tag Text" do
+      assert_equal find_text("%span Text to find"), ["Text to find"]
     end
 
-    test "else after nested loud script is accepted" do
-      begin
-        result = find_text "-if true\n  =if true\n    - 'A'\n-else\n  B"
-        assert_equal result, true
-      rescue SyntaxError
-        flunk 'else after nested loud script should be accepted'
-      end
+    test "%tag Text with class" do
+      assert_equal find_text("%span.whatever-with-thing Text to find"), ["Text to find"]
     end
 
-    test "case with indented whens should allow else" do
-      begin
-        result = find_text "- foo = 1\n-case foo\n  -when 1\n    A\n  -else\n    B"
-        assert_equal result, true
-      rescue SyntaxError
-        flunk 'case with indented whens should allow else'
-      end
+    test "%tag Text with ruby eval code" do
+      assert_equal find_text("%span= ruby_eval_code"), []
     end
+
+    test "%tag Text with ruby eval code and class" do
+      assert_equal find_text( "%span#whatever-with-thing= ruby_eval_code"), []
+    end
+    
+    test "Ruby style tags %a{'href' => 'http://whatever'} whatever" do
+      assert_equal find_text("%a{'href' => 'http://whatever'} whatever"), ["whatever"]
+    end
+    
+    # loud scripts / ruby eval mode
+    test "loud scripts with strings" do
+      assert_equal find_text('= "bob"'), ["bob"]
+      assert_equal find_text("= 'bob'"), ["bob"]
+    end
+
+    test "loud scripts does not interpolate ruby vars in strings" do
+      assert_equal find_text('= "ruby can #{var}"'), ["ruby can \#{var}"]
+    end
+    
+    # special loud scripts exceptions
+    test "it finds link_to texts as an exception to the rule" do
+      assert_equal find_text('= link_to "This should be found", "/"'), ["This should be found"]
+    end
+    
+    test "it finds link_to texts as an exception to the rule and does not interpolate" do
+      assert_equal find_text('= "Statistics for #{@name}"'), ["Statistics for \#{@name}"]
+    end
+
+    # html element mode with ruby evaling
+    test "html element with ruby eval with strings" do
+      assert_equal find_text('%p= "bob"'), ["bob"]
+      assert_equal find_text("%p.what= 'bob'"), ["bob"]
+      assert_equal find_text("%p.what{:attr => :val}= 'bob'"), ["bob"]
+    end
+    
+    test "html element loud scripts does not interpolate ruby vars in strings" do
+      assert_equal find_text('%p= "ruby can #{var}"'), ["ruby can \#{var}"]
+      assert_equal find_text('%p.what= "ruby can #{var}"'), ["ruby can \#{var}"]
+      assert_equal find_text('%p.what{:attr => :val}= "ruby can #{var}"'), ["ruby can \#{var}"]
+    end
+    
+    test "html element it finds link_to texts as an exception to the rule" do
+      assert_equal find_text('%p= link_to "This should be found", "/"'), ["This should be found"]
+      assert_equal find_text('%p.what= link_to "This should be found", "/"'), ["This should be found"]
+      assert_equal find_text('%p.what{:attr => :val}= link_to "This should be found", "/"'), ["This should be found"]
+    end
+    
+    test "html element it finds link_to texts as an exception to the rule and does not interpolate" do
+      assert_equal find_text('%p= "Statistics for #{@name}"'), ["Statistics for \#{@name}"]
+      assert_equal find_text('%p.what= "Statistics for #{@name}"'), ["Statistics for \#{@name}"]
+      assert_equal find_text('%p.what{:attr => :val}= "Statistics for #{@name}"'), ["Statistics for \#{@name}"]
+    end
+    
+    
+    
+    
+    
+    # test "TODOS" do
+    #   raise # make sure we test 
+    #   # (1) html tags
+    #   # 
+    #   # %li= link_to "Support Dashboard", EY::Zendesk.gateway_url
+    #   #  # this should work for passing in that ruby eval string as an arg to t('key', :name => name)
+    #   
+    #   # TODO when translating the text found to t() make sure to take into account duplicates of the same entry.
+    # end
 
     private
 
     def find_text(haml)
-      find_text = Parser::FindText.new(haml)
+      kinda_debug(haml) if ENV['DEBUG']
+      find_text = FindText.new(haml)
       find_text.run
+    end
+
+    def kinda_debug(haml)
+      begin
+        6.times do puts ; end
+        puts haml
+        2.times do puts ; end
+        r = Haml::Engine.new(haml).render
+        puts r
+        6.times do puts; end
+      rescue => e
+        puts e.message
+      end
     end
 
   end
